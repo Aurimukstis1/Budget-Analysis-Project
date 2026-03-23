5aad3bd369767542f0cd8657bc76df6b59fd37f6
Isaja Auguste <isaja.krisciunaite@stud.techin.lt>
2026-03-23 11:31:49 +0200

    created services folder, added a small bit of code to income_repository

---
+++ backend/repositories/income_repository.py
@@ -0,0 +1,13 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from models import Income
+
+class IncomeRepository:
+    @classmethod
+    async def create(db: AsyncSession, data: dict) -> Income:
+        income = Income(**data)
+        db.add(income)
+        await db.commit()
+        await db.refresh(income)
+        return income
---
+++ backend/services/__init__.py
@@ -0,0 +1,4 @@
+from .expense_service import ExpenseService 
+from .income_service import IncomeService 
+
+__all__ = ["ExpenseService", "IncomeService"]
